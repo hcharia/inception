d2b1a2917b887bce3848e74a24664c775957a78c
hcharia <58904206+hcharia@users.noreply.github.com>
2024-06-12 14:15:07 +0100

    Add files via upload

--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sleep 3
+sleep 5
 
 if [ ! -f "wp-config.php" ]; then
-wp config create --allow-root --dbname="${DATABASE_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="mariadb:3306"
+wp config create --allow-root --dbname="${DATABASE_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="mariadb"
 wp core install --url="${DN}" --title="${TITLE}" --admin_user="${WP_USER}" --admin_password="${WP_PASSWORD}" --admin_email="${WP_EMAIL}" --allow-root
 wp user create "${WP_MAN}" "${WP_MANMAIL}" --user_pass="${WP_MANPASS}" --role=editor --allow-root
 fi
